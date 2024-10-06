7f002eaca6372aba4f09046b6fef41fa49c832b9
SreeSagar09 <sreesagarmamidala09@gmail.com>
2024-10-06 22:03:38 +0530

    try catch blocks are added.

--- src/main/java/com/app/controller/ApplicationController1.java
+++ src/main/java/com/app/controller/ApplicationController1.java
@@ -16,62 +16,94 @@ public class ApplicationController1 {
 	
 	@RequestMapping(path = "/handlerMethod1/{variable}")
 	public ResponseEntity<String> handlerMethod1(@MatrixVariable String variable){
-		ResponseEntity<String> responseEntity = new ResponseEntity<String>(variable, HttpStatus.OK);
-		
+		ResponseEntity<String> responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String>(variable, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = "/handlerMethod2/{variables}")
 	public ResponseEntity<String[]> handlerMethod2(@MatrixVariable String variable1, @MatrixVariable String variable2){
-		String[] variables = {variable1, variable2};
-		ResponseEntity<String[]> responseEntity = new ResponseEntity<String[]>(variables, HttpStatus.OK);
-		
+		ResponseEntity<String[]> responseEntity = null;
+		try {
+			String[] variables = {variable1, variable2};
+			responseEntity = new ResponseEntity<String[]>(variables, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String[]>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = "/handlerMethod3/{variables}")
 	public ResponseEntity<String[]> handlerMethod3(@MatrixVariable(name = "variable") String[] variables){
-		ResponseEntity<String[]> responseEntity = new ResponseEntity<String[]>(variables, HttpStatus.OK);
-		
+		ResponseEntity<String[]> responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String[]>(variables, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String[]>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = "/handlerMethod4/{variables}")
 	public ResponseEntity<Map<String, Object>> handlerMethod4(@MatrixVariable Map<String, Object> map){
-		ResponseEntity<Map<String, Object>> responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
-		
+		ResponseEntity<Map<String, Object>> responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = "/handlerMethod5/{variables}")
 	public ResponseEntity<MultiValueMap<String, Object>> handlerMethod5(@MatrixVariable MultiValueMap<String, Object> multiValueMap){
-		ResponseEntity<MultiValueMap<String, Object>> responseEntity = new ResponseEntity<MultiValueMap<String, Object>>(multiValueMap, HttpStatus.OK);
-		
+		ResponseEntity<MultiValueMap<String, Object>> responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<MultiValueMap<String, Object>>(multiValueMap, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<MultiValueMap<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = "/handlerMethod6/{variables1}/{variables2}")
 	public ResponseEntity<Map<String, Object>> handlerMethod6(@MatrixVariable(pathVar = "variables1") Map<String, Object> map1, @MatrixVariable(pathVar = "variables2") Map<String, Object> map2){
-		Map<String, Object> map = new LinkedHashMap<>();
-		map.put("variables1", map1);
-		map.put("variables2", map2);
-		
-		ResponseEntity<Map<String, Object>> responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
-		
+		ResponseEntity<Map<String, Object>> responseEntity = null;
+		try {
+			Map<String, Object> map = new LinkedHashMap<>();
+			map.put("variables1", map1);
+			map.put("variables2", map2);
+			
+			responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = { "/handlerMethod7/{variable}", "/handlerMethod7" })
 	public ResponseEntity<String> handlerMethod7(@MatrixVariable(defaultValue = "No Matrix Variable") String variable){
-		ResponseEntity<String> responseEntity = new ResponseEntity<String>(variable, HttpStatus.OK);
-		
+		ResponseEntity<String> responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String>(variable, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = { "/handlerMethod8/{variable}", "/handlerMethod8" })
 	public ResponseEntity<String> handlerMethod8(@MatrixVariable(required = false) String variable){
-		ResponseEntity<String> responseEntity = new ResponseEntity<String>(variable, HttpStatus.OK);
-		
+		ResponseEntity<String> responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String>(variable, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 }
--- src/main/java/com/app/controller/ApplicationController2.java
+++ src/main/java/com/app/controller/ApplicationController2.java
@@ -16,32 +16,44 @@ public class ApplicationController2 {
 	
 	@RequestMapping(path = "/handlerMethod1/{pathVariable}/{matrixVariable}")
 	public ResponseEntity<Map<String, Object>> handlerMethod1(@PathVariable String pathVariable, @MatrixVariable Map<String, Object> matrixVariable){
-		Map<String, Object> map = new LinkedHashMap<>();
-		map.put("pathVariable", pathVariable);
-		map.put("matrixVariable", matrixVariable);
-		ResponseEntity<Map<String, Object>> responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
-		
+		ResponseEntity<Map<String, Object>> responseEntity = null;
+		try {
+			Map<String, Object> map = new LinkedHashMap<>();
+			map.put("pathVariable", pathVariable);
+			map.put("matrixVariable", matrixVariable);
+			responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = "/handlerMethod2/{variables1}")
 	public ResponseEntity<Map<String, Object>> handlerMethod2(@MatrixVariable Map<String, Object> map1){
-		Map<String, Object> map = new LinkedHashMap<>();
-		map.put("variables1", map1);
-		
-		ResponseEntity<Map<String, Object>> responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
-		
+		ResponseEntity<Map<String, Object>> responseEntity = null;
+		try {
+			Map<String, Object> map = new LinkedHashMap<>();
+			map.put("variables1", map1);
+			
+			responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@RequestMapping(path = "/handlerMethod2/{variables1}/{variables2}")
 	public ResponseEntity<Map<String, Object>> handlerMethod3(@MatrixVariable(pathVar = "variables1") Map<String, Object> map1, @MatrixVariable(pathVar = "variables2") Map<String, Object> map2){
-		Map<String, Object> map = new LinkedHashMap<>();
-		map.put("variables1", map1);
-		map.put("variables2", map2);
-		
-		ResponseEntity<Map<String, Object>> responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
-		
+		ResponseEntity<Map<String, Object>> responseEntity = null;
+		try {
+			Map<String, Object> map = new LinkedHashMap<>();
+			map.put("variables1", map1);
+			map.put("variables2", map2);
+			
+			responseEntity = new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 
